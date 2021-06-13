7bf6763025cffc7fbe9bbd4d700fa6416cb107ed
Jamie Stephens <71529189+jstephens@users.noreply.github.com>
2021-06-13 00:01:51 -0400

    new

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,12 @@ Created on Fri Jun  4 13:05:12 2021
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
